f5cc3e9327dcf11a7b4a1319a09f871423a4295b
mocsheng@foxmail.com <mo123456789>
2019-10-17 23:23:21 +0800

    添加servlet注解配置

--- src/com/m4coding/chatroom/servlet/LoginAction.java
+++ src/com/m4coding/chatroom/servlet/LoginAction.java
@@ -1,12 +1,13 @@
 package com.m4coding.chatroom.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet("/LoginAction")
 public class LoginAction extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
