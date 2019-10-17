b5770b4e1b036f7ec57772bf509a87a7b24afe3d
mochangsheng <mochangsheng>
2019-10-17 18:15:45 +0800

    添加servlet api包

---
+++ src/com/m4coding/chatroom/servlet/LoginAction.java
@@ -0,0 +1,15 @@
+package com.m4coding.chatroom.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class LoginAction extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
